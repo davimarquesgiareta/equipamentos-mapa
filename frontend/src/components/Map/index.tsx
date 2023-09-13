7a91c1740f495a584754484cc45413261d182294
Davi Marques <davi.giareta@nubbi.com.br>
2023-09-13 17:36:54 -0300

    adicionando marcadores e popups dinamicos

--- frontend/src/components/Map/index.tsx
+++ frontend/src/components/Map/index.tsx
@@ -3,6 +3,21 @@ import 'leaflet/dist/leaflet.css';
 
 export function Map() {
 
+    const markers = [
+        {
+            geocode: [48.86, 2.3522],
+            popup: "Pop up 1"
+        },
+        {
+            geocode: [48.85, 2.3522],
+            popup: "Pop up 2"
+        },
+        {
+            geocode: [48.855, 2.34],
+            popup: "Pop up 3"
+        }
+    ]
+
     return (
         <>
             <MapContainer center={[48.8566, 2.3522]} zoom={13} scrollWheelZoom={true} className='h-screen' >
@@ -10,11 +25,13 @@ export function Map() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[48.8566,2.3522]}>
-                    <Popup>
-                        alou
-                    </Popup>
-                </Marker>
+                { markers.map(marker => (
+                    <Marker position={marker.geocode}>
+                        <Popup>
+                            <h2>{marker.popup}</h2>
+                        </Popup>
+                    </Marker>
+                )) }
             </MapContainer>
         </>
     )
